5165dbf77556392989a64c39fd248e9d9e661f98
nathan <slophil.contact@gmail.com>
2025-09-02 17:57:28 +0200

    FEAT: Initial Commit

---
+++ app/src/application_core/domain/entities/praticien/Praticien.php
@@ -0,0 +1,130 @@
+<?php
+
+namespace toubilib\core\domain\entities\praticien;
+
+
+class Praticien
+{
+//    string id
+//    string nom
+//    string prenom
+//    string ville
+//    string email
+//    string telephone
+//    string rpps_id
+//    string titre
+//    boolean accepte_nouveau_patient
+//    boolean est_organisation
+
+    private string $id;
+    private string $nom;
+    private string $prenom;
+    private string $ville;
+    private string $email;
+    private string $telephone;
+    private string $rpps_id;
+    private string $titre;
+    private bool $accepte_nouveau_patient;
+    private bool $est_organisation;
+
+
+    public function isEstOrganisation(): bool
+    {
+        return $this->est_organisation;
+    }
+
+    public function setEstOrganisation(bool $est_organisation): void
+    {
+        $this->est_organisation = $est_organisation;
+    }
+
+    public function isAccepteNouveauPatient(): bool
+    {
+        return $this->accepte_nouveau_patient;
+    }
+
+    public function setAccepteNouveauPatient(bool $accepte_nouveau_patient): void
+    {
+        $this->accepte_nouveau_patient = $accepte_nouveau_patient;
+    }
+
+    public function getTitre(): string
+    {
+        return $this->titre;
+    }
+
+    public function setTitre(string $titre): void
+    {
+        $this->titre = $titre;
+    }
+
+    public function getRppsId(): string
+    {
+        return $this->rpps_id;
+    }
+
+    public function setRppsId(string $rpps_id): void
+    {
+        $this->rpps_id = $rpps_id;
+    }
+
+    public function getTelephone(): string
+    {
+        return $this->telephone;
+    }
+
+    public function setTelephone(string $telephone): void
+    {
+        $this->telephone = $telephone;
+    }
+
+    public function getEmail(): string
+    {
+        return $this->email;
+    }
+
+    public function setEmail(string $email): void
+    {
+        $this->email = $email;
+    }
+
+    public function getVille(): string
+    {
+        return $this->ville;
+    }
+
+    public function setVille(string $ville): void
+    {
+        $this->ville = $ville;
+    }
+
+    public function getPrenom(): string
+    {
+        return $this->prenom;
+    }
+
+    public function setPrenom(string $prenom): void
+    {
+        $this->prenom = $prenom;
+    }
+
+    public function getNom(): string
+    {
+        return $this->nom;
+    }
+
+    public function setNom(string $nom): void
+    {
+        $this->nom = $nom;
+    }
+
+    public function getId(): string
+    {
+        return $this->id;
+    }
+
+    public function setId(string $id): void
+    {
+        $this->id = $id;
+    }
+}
